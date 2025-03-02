8f1f2f0be2f471410ad0e8a6de5bc0fb71e36aa8
Aryan Nayak <aryannayak9131@gmail.com>
2025-03-02 11:35:06 +0530

    Removed vercel.json and updated new backend url

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://chess-app-backend-rosy.vercel.app',
+        target: 'https://chess-app-backend-c3zs.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
