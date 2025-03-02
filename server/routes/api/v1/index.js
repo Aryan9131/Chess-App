ad7b49edb38ae558ac12c2a478dfb0cdbe225bf8
Aryan Nayak <aryannayak9131@gmail.com>
2025-03-02 11:13:49 +0530

    Added test route and start command

--- server/routes/api/v1/index.js
+++ server/routes/api/v1/index.js
@@ -4,6 +4,7 @@ const GameController = require('../../../Controllers/GameController')
 router.get('/new-game',GameController.startGame)
 router.post('/player-move',GameController.playerMove)
 router.get('/ai-move',GameController.aiMove)
+
 router.get('/test',(req, res)=>{
     return res.status(200).json({
         message:'got the api !'
